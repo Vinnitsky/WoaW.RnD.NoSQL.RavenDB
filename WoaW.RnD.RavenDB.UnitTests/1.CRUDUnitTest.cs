eaff1463bf8a422df6f312a50a25a7b4ce2e3695
vinnitsky <vinnitsky@msn.com>
2014-08-17 20:57:46 +0300

    проапдейтил сервер

--- WoaW.RnD.RavenDB.UnitTests/1.CRUDUnitTest.cs
+++ WoaW.RnD.RavenDB.UnitTests/1.CRUDUnitTest.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Raven.Client;
 using Raven.Client.Document;
 using WoaW.RnD.RavenDB.UnitTests.Entities;
 using System.Linq;
@@ -11,27 +10,15 @@ namespace WoaW.RnD.RavenDB.UnitTests
     /// класс создан для демонстрации реализации CRUD
     /// </summary>
     [TestClass]
-    public class CRUDUnitTest
+    public class CrudUnitTest
     {
-        private TestContext testContextInstance;
 
+        #region Additional test attributes
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
         ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
+        public TestContext TestContext { get; set; }
         //
         // You can use the following additional attributes as you write your tests:
         //
@@ -52,7 +39,6 @@ namespace WoaW.RnD.RavenDB.UnitTests
         // public void MyTestCleanup() { }
         //
         #endregion
-
 
         /// <summary>
         /// метод демонстрирует подключения. кстрока соединения увлючена в код.  
@@ -135,13 +121,27 @@ namespace WoaW.RnD.RavenDB.UnitTests
         [TestCategory("CRUD")]
         public void GetElementById_SuccessTest()
         {
+            //arrange
+            using (var store = new DocumentStore {ConnectionStringName = "ravenDB"}.Initialize())
+            {
+                using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
+                {
+                    var customer = new Customer { Id = 1.ToString(CultureInfo.InvariantCulture), Title = "Mr.", Name = "Who ", Email = "who@live.com" };
+                    session.Store(customer);
+                    session.SaveChanges();
+                }
+            }
+
             using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
             {
                 using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
                 {
-                    var customer = session.Load<Customer>("customers/1");
-                    System.Diagnostics.Debug.WriteLine(string.Format("CUSTOMER: Id={0}, Name={1}, Email={2}",
-                        customer.Id, customer.Name, customer.Email));
+                    //act
+                    var customer = session.Load<Customer>("1");
+
+                    //assert
+                    Assert.IsNotNull(customer);
+                    System.Diagnostics.Debug.WriteLine("CUSTOMER: Id={0}, Name={1}, Email={2}", customer.Id, customer.Name, customer.Email);
                 }
             }
         }
@@ -153,13 +153,27 @@ namespace WoaW.RnD.RavenDB.UnitTests
         [TestCategory("CRUD")]
         public void CreateNewCustomer_SuccessTest()
         {
+            //arrange
             using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
             {
                 using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
                 {
-                    var customer = new Customer() { Id = 1.ToString(), Title = "Mr.", Name = "Who ", Email = "who@live.com" };
+                    //act
+                    var customer = new Customer { Id = 1.ToString(CultureInfo.InvariantCulture), Title = "Mr.", Name = "Who ", Email = "who@live.com" };
                     session.Store(customer);
                     session.SaveChanges();
+                }
+            }
+
+            using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
+            {
+                using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
+                {
+                    var customer = session.Load<Customer>("1");
+
+                    //assert
+                    Assert.IsNotNull(customer);
+                    System.Diagnostics.Debug.WriteLine("CUSTOMER: Id={0}, Name={1}, Email={2}", customer.Id, customer.Name, customer.Email);
                 }
             }
         }
@@ -179,6 +193,19 @@ namespace WoaW.RnD.RavenDB.UnitTests
                     session.SaveChanges();
                 }
             }
+
+            using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
+            {
+                using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
+                {
+                    //act
+                    var customer = session.Load<Customer>("1");
+
+                    //assert
+                    Assert.IsNull(customer);
+                }
+            }
+
         }
 
         /// <summary>
@@ -193,14 +220,59 @@ namespace WoaW.RnD.RavenDB.UnitTests
                 using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
                 {
                     var customer = session.Load<Customer>("customers/1");
-                    System.Diagnostics.Debug.WriteLine(string.Format("CUSTOMER before update: Id={0}, Title={1}, Name={2}, Email={3}",
-                        customer.Id, customer.Title, customer.Name, customer.Email));
+                    System.Diagnostics.Debug.WriteLine("CUSTOMER before update: Id={0}, Title={1}, Name={2}, Email={3}", 
+                        customer.Id, customer.Title, customer.Name, customer.Email);
 
                     customer.Title = "PhD";
                     session.SaveChanges();
 
-                    System.Diagnostics.Debug.WriteLine(string.Format("CUSTOMER after update: Id={0}, Title={1}, Name={2}, Email={3}",
-                        customer.Id, customer.Title, customer.Name, customer.Email));
+                    System.Diagnostics.Debug.WriteLine("CUSTOMER after update: Id={0}, Title={1}, Name={2}, Email={3}", 
+                        customer.Id, customer.Title, customer.Name, customer.Email);
+                }
+            }
+        }
+        /// <summary>
+        /// метод демоснтрирует обновление через использование одного идентификатора 
+        /// </summary>
+        [TestMethod]
+        [TestCategory("CRUD")]
+        public void Update_UsingSameIdSuccessTest()
+        {
+            //arrange
+            using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
+            {
+                using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
+                {
+                    var customer = new Customer { Id = 1.ToString(CultureInfo.InvariantCulture), Title = "Mr.", Name = "Who ", Email = "who@live.com" };
+                    session.Store(customer);
+                    session.SaveChanges();
+
+                    System.Diagnostics.Debug.WriteLine("CUSTOMER after update: Id={0}, Title={1}, Name={2}, Email={3}",
+                        customer.Id, customer.Title, customer.Name, customer.Email);
+                }
+            }
+
+            using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
+            {
+                using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
+                {
+                    var customer = new Customer { Id = 1.ToString(CultureInfo.InvariantCulture), Title = "PhD.", Name = "Who ", Email = "who@live.com" };
+                    session.Store(customer);
+                    session.SaveChanges();
+
+                    System.Diagnostics.Debug.WriteLine("CUSTOMER after update: Id={0}, Title={1}, Name={2}, Email={3}", 
+                        customer.Id, customer.Title, customer.Name, customer.Email);
+                }
+            }
+
+            //assert
+            using (var store = new DocumentStore { ConnectionStringName = "ravenDB" }.Initialize())
+            {
+                using (var session = store.OpenSession("WoaW.Raven.FirstApp"))
+                {
+                    var customer = session.Load<Customer>("1");
+                    Assert.IsNotNull(customer);
+                    Assert.AreEqual("PhD.", customer.Title);
                 }
             }
         }
